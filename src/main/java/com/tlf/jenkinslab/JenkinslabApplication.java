93b9c5b604f6372ad157a3b0c75aa1f89cdfac11
m4ndr4ck <davi@hospedax.com>
2018-11-30 01:56:15 -0200

    First commit

---
+++ src/main/java/com/tlf/jenkinslab/JenkinslabApplication.java
@@ -0,0 +1,12 @@
+package com.tlf.jenkinslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinslabApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tlf/jenkinslab/JenkinslabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tlf.jenkinslab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinslabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
